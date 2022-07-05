02f35514a09e33a1358b1d7aaa3e37b9248313fb
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-07-05 14:06:21 -0300

    testing database

---
+++ atv-tests-3/app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksLocalDataSourceTest.kt
@@ -0,0 +1,92 @@
+package com.example.android.architecture.blueprints.todoapp.data.source.local
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.succeeded
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@MediumTest
+class TasksLocalDataSourceTest {
+
+    private lateinit var localDataSource: TasksLocalDataSource
+    private lateinit var database: ToDoDatabase
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setup() {
+        // Using an in-memory database for testing, because it doesn't survive killing the process.
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            ToDoDatabase::class.java
+        )
+            .allowMainThreadQueries()
+            .build()
+
+        localDataSource =
+            TasksLocalDataSource(
+                database.taskDao(),
+                Dispatchers.Main
+            )
+    }
+
+    //cleaning the database after using it
+    @After
+    fun cleanUp(){
+        database.close()
+    }
+
+// TODO: Replace with runBlockingTest once issue is resolved
+    @Test
+    fun saveTask_retrievesTask() = runBlocking {
+        // GIVEN - A new task saved in the database.
+        val newTask = Task("title", "description", false)
+        localDataSource.saveTask(newTask)
+
+        // WHEN  - Task retrieved by ID.
+        val result = localDataSource.getTask(newTask.id)
+
+        // THEN - Same task is returned.
+        assertThat(result.succeeded, `is`(true))
+        result as Result.Success
+        assertThat(result.data.title, `is`("title"))
+        assertThat(result.data.description, `is`("description"))
+        assertThat(result.data.isCompleted, `is`(false))
+    }
+
+    @Test
+    fun completeTask_retrievedTaskIsComplete() = runBlocking{
+        // 1. Save a new active task in the local data source.
+        val saveTask = Task("title")
+        localDataSource.saveTask(saveTask)
+
+        // 2. Mark it as complete.
+        localDataSource.completeTask(saveTask)
+        val result = localDataSource.getTask(saveTask.id)
+
+        // 3. Check that the task can be retrieved from the local data source and is complete.
+        assertThat(result.succeeded, `is`(true))
+        result as Result.Success
+        assertThat(result.data.title, `is`(saveTask.title))
+        assertThat(result.data.isCompleted, `is`(true))
+
+    }
+}
